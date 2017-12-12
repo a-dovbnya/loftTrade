4ccfcd7113baea50d8a70db9e953ab57a6b3b346
Довбня Алексей <dovbnya@intaro.ru>
2017-12-13 02:41:28 +0300

    test deploy 2

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ zip -r build.zip build
 curl -H "Content-type: application/zip" \
      -H "Authorization: Bearer $NETLIFY_KEY" \
      --data-binary "@build.zip" \
-https://api.netlify.com/api/v1/sites/
+https://api.netlify.com/api/v1/sites/a8532673-a1c9-4618-af5e-a006bb65b8ec/deploy
